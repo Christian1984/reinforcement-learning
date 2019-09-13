1117694d2443de27a4856cbcb2c41a73a4e867ee
Chris <chris.hoffmann84@gmail.com>
2019-09-13 10:59:54 +0200

    added simplified movement for target

--- lasergame/ufo.py
+++ lasergame/ufo.py
@@ -2,14 +2,26 @@ class Ufo:
     def __init__(self, x = 0.5, y = 0.75, radius = 0.05, velocity = 0.01):
         self.x = x
         self.y = y
-        self.target_pos_x = x
-        self.target_pos_y = y
+        self.target_x = x
+        self.target_y = y
         self.radius = radius
+        self.velocity = velocity
 
     def update(self):
-        #TODO: move towards ufo until distance < sqrt(2v^2)
+        self.x = self.__apply_velocity(self.x, self.target_x)
+        self.y = self.__apply_velocity(self.y, self.target_y)
+
+        #TODO: normalize
+        
         return
     
     def set_target_pos(self, x, y):
-        self.target_pos_x = x
-        self.target_pos_y = y
+        self.target_x = x
+        self.target_y = y
+
+    def __apply_velocity(self, pos, target_pos):
+        if abs(pos - target_pos) < self.velocity:
+            return target_pos
+
+        dir = 1 if target_pos > pos else -1
+        return pos + dir * self.velocity
